3cb686f1b6ddb482d0aff19c11cf390797aa3d29
Aamir khan <aamirkhan180@gmail.com>
2017-01-24 11:28:29 +0530

    tweaks and pepify

--- Python_commandline_youtube_downloader.py
+++ Python_commandline_youtube_downloader.py
@@ -1,29 +1,27 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
 """
 Commandline_youtube_downloader
 ============================================
 Coded By: Jackson Kamya
 Email: jacksonkamya48@yahoo.com
-
 This script downloads Music from youtube
-
 Pre-Requirements:
 =====================
 Python 2.7
 BeautifulSoup: https://www.crummy.com/software/BeautifulSoup/bs4/download/
 pytube: www.pypi.python.org/pypi/pytube/ | www.sourceforge.net/projects/pytube/
-
 Features:
 =========
 1. Download videos one by one as searched at a prompt
-2. Download a list of songs as read line by line from file todownload.txt
-
+2. Download a list of songs as read line by line from txt file
 You are free to copy and modify this script for your own requirements.
-
 """
 
 import os
 import time
-from urllib2 import urlopen
+from urllib2 import urlopen, Request
 import sys
 
 try:
@@ -31,43 +29,27 @@ try:
     from pytube import YouTube
 except:
     print('Please install BeautifulSoup and pytube to run this script')
-    # exit is mean to work with intrepter must use sys.exit
     sys.exit(0)
 
 # creating download folder
-# output_folder = 'D:/py_jack_downloads'
 base_dir = os.path.expanduser("~")
-output_folder = os.path.join(base_dir, "Videos", "py_jack_downloads")
+output_folder = os.path.join(base_dir, "Downloads", "py_cmd_utube_downloads")
 
 if not os.path.exists(output_folder):
     os.mkdir(output_folder)
-
-# it's better to ask from the user to provide the path
-input_file = "D:/todownload.txt"
-
-# Selecting mode of operation
-while True:
-    print("A >>> Search songs from the prompt")
-    print("B >>> Download songs in the todownload file")
-
-    mode = raw_input('Select Mode: A or B > ').lower()
-    if len(mode) > 1 or not (mode.startswith('a') or mode.startswith("b")):
-        print('Invalid mode, please try again')
-        continue
-    break
 
 
 # Getting youtube search term
 def get_search_term():
     while True:
-        inpt = raw_input('Enter search term >> ').lower()
+        # line is 92 char long pepify it
+        inpt = raw_input('Enter search term\nor press <<Enter>> to change mode: > ').lower()
         if not inpt:
-            print('You didn\'t give us a thing, please try again')
-            continue
-        srch_terms = inpt.split(' ')
-        search = '+'.join(srch_terms)
+            return main('b')
+        # search = inpt.replace(" ", "+")
+        # search = '+'.join(srch_terms)
         break
-    return search
+    return inpt.replace(" ", "+")
 
 
 # downloading the file
@@ -76,11 +58,8 @@ def download(url):
     for resol in accepted_resolutions:
         try:
             vid = YouTube(url).get('mp4', resol)
-            # what is the point of breaking this here ?
-            # taking the first resol which is 480p and exit ?
             break
         except Exception:
-            # what is the point of this check ?
             if resol not in accepted_resolutions:
                 print('No video matching any of the qualities required')
                 return False
@@ -110,17 +89,17 @@ def select_video(soup):
         print(str(count) + " >>> " + t)
     while True:
         try:
-            # fix pep 8 E501
-            selection = int(raw_input('Select from the above list or enter 0 to return to main menu >'))
-            if(selection >= count):
+            # line 108 char ling pepify it
+            selection = int(raw_input('Select from the above list or enter 0 to return to main program > '))
+            if(selection > count):
                 raise ValueError
             elif(selection == 0):
                 return main(mode)
             else:
                 return video_list[selection - 1]
         except ValueError:
-            # fix pep 8 E501
-            print('Input must be a number in between 1-{}, please try again'.format(len(video_list)))
+            # line 92 char long pepify it
+            print('Input must be a number in between 1-{}, please try again '.format(count))
 
 
 # Getting the file
@@ -136,10 +115,8 @@ def get_video(soup):
                 end_time = time.time()
                 mins = (end_time - start_time) / 60
                 secs = (end_time - start_time) % 60
-                # avoid '+' use string formatting instead
-                # fix pep 8 E501
-                dur = str(round(mins)) + " minutes, " + str(round(secs)) + " seconds"
-                print("Downloaded {0} in {1}".format(title, dur))
+                # line 88 char long pepify it
+                print("Downloaded %s in %.0f minutes, %.0f seconds" % (title,mins,secs))
             else:
                 print("Could not download the video")
         except Exception as e:
@@ -150,9 +127,13 @@ def get_video(soup):
 def downloader(term):
     # Openning youtube search page
         try:
+            agent = ("Mozilla/5.0 "
+                     "(X11; Ubuntu; Linux x86_64; rv:50.0) "
+                     "Gecko/20100101 Firefox/50.0")
+
             url = 'https://www.youtube.com/results?search_query=' + term
-            with urlopen(url) as r:
-                html = r.read()
+            req = Request(url, headers={'User-Agent': agent})
+            html = urlopen(req).read()
         except Exception as e:
             print(e)
             return
@@ -172,12 +153,54 @@ def downloader(term):
             return
 
 
+# Selecting mode of operation
+def get_mode():
+    while True:
+        print("A >>> Search videos from the prompt")
+        print("B >>> Download videos from a file")
+        global mode
+        mode = raw_input('Select Mode: A or B > ').lower()
+        if len(mode) > 1 or not (mode.startswith('a') or mode.startswith("b")):
+            print('Invalid mode, please try again')
+            continue
+        break
+    return mode
+
+
 # Main function of the program
-def main(mode):
+# passing None seems useless we can pass default mode
+def main(m=None):
+    if not m:
+        mode = get_mode()
+    else:
+        mode = m
     if(mode == 'a'):
         while True:
             downloader(get_search_term())
     else:
+        input_file = raw_input('Provide input file path e.g. D:/input.txt > ')
+        if not input_file:
+            print("Invalid path, please try again")
+            return main(mode)
+        else:
+            if not os.path.exists(input_file):
+                print("File does not exist")
+                while True:
+                    print("A >> switch mode")
+                    print("B >> Try again")
+                    print("C >> Quit program")
+
+                    choice = raw_input('Select one of the above options > ')
+                    if choice.lower() not in ('a', 'b', 'c'):
+                        print('Invalid choice, please try again')
+                        continue
+                    # can we make it more pythonic ?
+                    if(choice == "a"):
+                        return main('a')
+                    elif(choice == 'b'):
+                        return main(mode)
+                    else:
+                        sys.exit(0)
         with open(input_file, 'r') as f:
             try:
                 files = f.readlines()
@@ -185,8 +208,8 @@ def main(mode):
                 print(e)
                 return
         for item in files:
-            downloader('+'.join(item.split(' ')))
+            downloader(item.replace(" ", "+"))
 
 
 if __name__ == "__main__":
-    main(mode)
+    main()
